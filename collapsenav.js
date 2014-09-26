0191dcc2ad714954bfd61c5e20166ca45c710214
stephansmith <hello@stephan-smith.com>
2014-09-26 13:23:52 -0500

    Added getComputedStyle polyfill

--- collapsenav.js
+++ collapsenav.js
@@ -2,6 +2,28 @@
 	
 	var collapseNav = function ( el, options ) {
 		
+		var computed = !!window.getComputedStyle;
+		
+		// getComputedStyle polyfill
+		if ( !computed ) {
+			window.getComputedStyle = function(el) {
+				this.el = el;
+				this.getPropertyValue = function(prop) {
+					var re = /(\-([a-z]){1})/g;
+					if (prop === "float") {
+						prop = "styleFloat";
+					}
+					if (re.test(prop)) {
+						prop = prop.replace(re, function () {
+							return arguments[2].toUpperCase();
+						});
+					}
+					return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+				};
+				return this;
+			};
+		}
+		
 		var addEvent = function (el, evt, fn, bubble) {
 			if ("addEventListener" in el) {
 				// BBOS6 doesn't support handleEvent, catch and polyfill
